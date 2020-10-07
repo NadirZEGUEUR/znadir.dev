9a4cabb0d5170f88c12593694c81c00888340ca0
Nadir ZEGUEUR <NadirZEGUEUR@users.noreply.github.com>
2020-10-08 01:07:06 +0200

    Obsolete solutions

--- script.js
+++ script.js
@@ -1,25 +1,92 @@
 
-
-
 // Gestion des sections
 
 let AllSections = document.querySelectorAll(".section");
 let section_QuiSuisJe = document.querySelector("#qui-suis-je");
 let currentSection = section_QuiSuisJe;
+let i = 0;
 var iterator = AllSections.values();
+
+// Redéfinition de l'itérateur pour inclure un hack permettant de parcourir la structure en arrière "Backward iteration"
+// DOES NOT FUNCTION
+/* AllSections.values = () => {
+  const items = [...AllSections.values()];
+  let index = -1;
+
+  return {
+    [Symbol.iterator]() {
+      return this;
+    },
+
+    next() {
+      var item = items[index + 1];
+      if (item) {
+        return {
+          value: item,
+          done: false
+        };
+      }
+      return { done: true };
+    },
+
+    previous() {
+      var item = items[index - 1];
+      if (item) {
+        return {
+          value: item,
+          done: false
+        };
+      }
+      return { done: true };
+    }
+  };
+} */
+
 
 function nextSection(){
   
+  if (currentSection != AllSections[AllSections.length-1]){
+    currentSection = currentSection.nextElementSibling;
+  }
+
+  /* if (currentSection == AllSections[AllSections.length-1]) {
+    Object.values(AllSections).reverse();
+    i = 0;
+  }else{
+    i++;
+  }
+  currentSection = AllSections[i]; */
+
+
   
-  if (!iterator.next().done){
+  console.log(currentSection);
+  /* if (!iterator.next().done){
     console.log(iterator.next());
   }else{
     
-  }
+  } */
     
 
   currentSection.scrollIntoView();
   return currentSection.scrollHeight;
+}
+
+function previousSection(){
+  
+  if (currentSection != section_QuiSuisJe){
+    currentSection = currentSection.previousElementSibling;
+  }
+  
+  //console.log(iterator.previous());
+  /* if (!iterator.previous().done){
+    console.log(iterator.next());
+  }else{
+    
+  } */
+}
+
+function goToSection(section){
+  section.scrollIntoView();
 }
 
 //  Animation tête
@@ -64,9 +131,10 @@ function showMaTete(){
 let lien_QuiSuisJe = document.querySelector("a[href='#qui-suis-je']");
 
 lien_QuiSuisJe.addEventListener("click", ()=>{
-    
-    showMaTete();
-   
+
+  goToSection(section_QuiSuisJe);
+  showMaTete();
+
 });
 
 window.addEventListener("scroll", ()=>{
@@ -82,7 +150,7 @@ window.addEventListener("scroll", ()=>{
       }
     }, 500);
 
-    window.removeEventListener("scroll");
+    //window.removeEventListener("scroll");
     //TODO add event listener when it goes back to the 1st section
     //el.removeEventListener("click", hideMaTete);
     //el.addEventListener("click", showMaTete);
